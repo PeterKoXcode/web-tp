e673fbd0acaf14449a3e7c0e7430c4ee6611f15b
peter <xkopecky@stuba.sk>
2023-10-31 20:38:23 +0100

    base added to browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/web-tp/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>

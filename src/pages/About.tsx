97187765a8d0d4974d048d7a3742890ef57de4af
peter <xkopecky@stuba.sk>
2023-10-31 18:05:41 +0100

    .env file added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,25 +5,25 @@ export function About() {
     <>
       <Container className="text-center">
         <h1 className="mb-5">
-            Segmentácia vrstiev sietnice oka z obrazov získaných optickou
-            koherentnou tomografiou (OCT)
+          Segmentácia vrstiev sietnice oka z obrazov získaných optickou
+          koherentnou tomografiou (OCT)
         </h1>
         <p>
-            Optická koherentná tomografia je neinvazívne zobrazovacie vyšetrenie
-            oka, ktoré nasníma jednotlivé rezy snímaného tkaniva na úrovni
-            mikrometrov s možnosťou vizualizácie jednotlivých vrstiev. Výsledkom sú
-            rezy tkanivom s možnosťou 3D rekonštrukcie obrazu, ako pri počítačovej
-            tomografii (CT), ale bez radiačnej záťaže. Úlohou tímového projektu je
-            spracovanie jednotlivých nasnímaných rezov centra sietnice, obsahujúcich
-            jej jednotlivé vrstvy, ich príprava a analýza za účelom segmentácie
-            týchto vrstiev. 1. Vyhľadajte voľne dostupné databázy OCT obrazov
-            sietnice. 2. Naštudujte si dostupné metódy segmentácie týchto dát. 3.
-            Podľa dostupných metód vyhľadajte metódy predspracovania, ak sú potrebné
-            a skúste niektoré implementovať. 4. Z naštudovaných riešení
-            implementujte vybrané riešenie segmentácie, prípadne navrhnite vlastné.
-            5. Otestujte vytvorené riešenie.
+          Optická koherentná tomografia je neinvazívne zobrazovacie vyšetrenie
+          oka, ktoré nasníma jednotlivé rezy snímaného tkaniva na úrovni
+          mikrometrov s možnosťou vizualizácie jednotlivých vrstiev. Výsledkom
+          sú rezy tkanivom s možnosťou 3D rekonštrukcie obrazu, ako pri
+          počítačovej tomografii (CT), ale bez radiačnej záťaže. Úlohou tímového
+          projektu je spracovanie jednotlivých nasnímaných rezov centra
+          sietnice, obsahujúcich jej jednotlivé vrstvy, ich príprava a analýza
+          za účelom segmentácie týchto vrstiev. 1. Vyhľadajte voľne dostupné
+          databázy OCT obrazov sietnice. 2. Naštudujte si dostupné metódy
+          segmentácie týchto dát. 3. Podľa dostupných metód vyhľadajte metódy
+          predspracovania, ak sú potrebné a skúste niektoré implementovať. 4. Z
+          naštudovaných riešení implementujte vybrané riešenie segmentácie,
+          prípadne navrhnite vlastné. 5. Otestujte vytvorené riešenie.
         </p>
-        <img src="/public/layers.png" alt="OCT layers" />
+        <img src="./public/layers.png" alt="OCT layers" />
       </Container>
     </>
   );

2918e49e2f1c66cd14031a1a2368ba9e4b3abb2e
peter <xkopecky@stuba.sk>
2023-10-31 17:45:52 +0100

    first commit using gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => {
-
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if(command !== 'serve') {
-    config.base = '/web-tp/'
-  }
-
-  return config
-
+export default defineConfig({
+  base: '/web-tp',
+  plugins: [react()],
 })

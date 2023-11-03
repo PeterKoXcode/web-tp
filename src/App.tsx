df2b20caa730d0f491bcfa42a8cbb171915b484e
peter <xkopecky@stuba.sk>
2023-11-03 11:59:57 +0100

    update data zápisnica, changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Container } from "react-bootstrap"
-import { Navbar } from "./components/Navbar"
-import { Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { About } from "./pages/About"
-import { MinuteBook } from "./pages/MinuteBook"
+import { Container } from "react-bootstrap";
+import { Navbar } from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { MinuteBook } from "./pages/MinuteBook";
 
-type Props = {}
+type Props = {};
 
 export function App({}: Props) {
   return (
     <>
-    <Navbar />
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/minute-book" element={<MinuteBook />} />
-      </Routes>
-    </Container>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/minute-book" element={<MinuteBook />} />
+        </Routes>
+      </Container>
     </>
-  )
+  );
 }

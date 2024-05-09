dca6d8337c0c3bdb71dbad0fceb17ffffb0fcd61
peter <xkopecky@stuba.sk>
2024-05-09 18:29:07 +0200

    about finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 //import { Container } from "react-bootstrap";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { MinuteBook } from "./pages/MinuteBook";
@@ -11,17 +11,13 @@ type Props = {};
 export function App({}: Props) {
   return (
     <>
-      <Router>
-        <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/minute-book" element={<MinuteBook />} />
-        </Routes>
-        </div>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/minute-book" element={<MinuteBook />} />
+      </Routes>
     </>
   );
 }

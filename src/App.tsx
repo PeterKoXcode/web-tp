c36f27c0c3ab0345ae58e2823d21ed3c5a37956e
peter <xkopecky@stuba.sk>
2024-05-09 17:54:39 +0200

    redirect issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 //import { Container } from "react-bootstrap";
 import { Navbar } from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { MinuteBook } from "./pages/MinuteBook";
@@ -11,13 +11,17 @@ type Props = {};
 export function App({}: Props) {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/minute-book" element={<MinuteBook />} />
-      </Routes>
+      <Router>
+        <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/minute-book" element={<MinuteBook />} />
+        </Routes>
+        </div>
+      </Router>
     </>
   );
 }
